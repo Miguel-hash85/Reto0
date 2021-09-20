9ce6238e0538d10befdeacf763f028af807575bc
matteo <46781101+matteofdez@users.noreply.github.com>
2021-09-20 14:00:42 +0200

    Update Aplication.java

--- src/reto0/Aplication.java
+++ src/reto0/Aplication.java
@@ -5,6 +5,10 @@
  */
 package reto0;
 
+import reto0.controller.Controller;
+import reto0.model.Model;
+import reto0.view.View;
+
 /**
  *
  * @author 2dam
@@ -15,7 +19,20 @@ public class Aplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //new
+        Model model = new Model() {
+        };
+        View view = new View() {
+            @Override
+            public void showGreeting(String greeting) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        Controller controller = new Controller();
+
+        // TODO: model=ModelFactory.getModelInstance().getModel(ModelFactory.TYPE);
+        // TODO: view=ViewFactory.getViewInstance().getView(ViewFactory.TYPE);
+        controller.run(model, view);
     }
-    
+
 }
